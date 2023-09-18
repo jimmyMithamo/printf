05fbe5b99a9c3e6806ab6dc246727f978b4f8e7f
jimmyMithamo <j1mithamo@gmail.com>
2023-09-18 22:43:21 +0300

    source file

---
+++ _print.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+  *_printf - produces output according to a format
+  *@format: is a character string
+  *
+  *Return: zero on success
+  */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				_putc('%');
+			}
+			else if (*format == 'c')
+			{
+				_putc(va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				fputs(va_arg(args, const char *), stdout);
+			}
+		}
+		else
+		{
+			_putc(*format);
+		}
+		format++;
+		count++;
+	}
+	
+	return (count);
+}
