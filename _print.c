6924320f195e303f2a84c9a6924f4d1d92e16587
Melody George <melodygeorge99@gmail.com>
2023-09-19 02:50:53 -0700

    tasks

--- _print.c
+++ _print.c
@@ -32,7 +32,7 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 's')
 			{
-				fputs (va_arg(args, const char *), stdout);
+				fputs(va_arg(args, const char *), stdout);
 			}
 		}
 		else
